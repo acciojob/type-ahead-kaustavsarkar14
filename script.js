b977a72b77f08da78c5e0c452d890daaffb9a64e
Kaustav Sarkar <93545845+kaustavsarkar14@users.noreply.github.com>
2023-12-14 11:31:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+const input = document.getElementById('typeahead')
+const ul =  document.getElementById('suggestions-list')
+let timer = null
+input.addEventListener('input',(e)=>{
+	clearTimeout(timer)
+	timer = setTimeout(()=>{
+		fetch(`https://api.frontendexpert.io/api/fe/glossary-suggestions?text=${e.target.value}`)
+		.then(data=>data.json())
+		.then(data=>{
+			ul.innerHTML = ''
+			data.forEach(el=>{
+				const li = document.createElement('li')
+				li.innerText = el
+				li.addEventListener('click',e=>{
+					input.value = e.target.innerText
+					ul.innerHTML = ''
+				})
+				ul.appendChild(li)
+			})
+		})
+	},500)
+})
